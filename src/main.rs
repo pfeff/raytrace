6215f89639f703cc5baefd40dda90f948b7dc132
Matt Pfefferle <27587+pfeff@users.noreply.github.com>
2018-08-27 20:06:13 -0400

    Move tests to module under test

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,3 @@ fn main() {
 }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn is_point() {
-        let a = tuple::Tuple { x: 4.3, y: -4.2, z: 3.1, w: 1.00000001 };
-        assert!(a.is_point());
-        assert!(!a.is_vector());
-    }
-
-    #[test]
-    fn is_vector() {
-        let a = tuple::Tuple { x: 4.3, y: -4.2, z: 3.1, w: 0.00000001 };
-        assert!(!a.is_point());
-        assert!(a.is_vector());
-    }
-
-    #[test]
-    fn new_vector() {
-        let v = tuple::vector(4.0, -4.0, 3.0);
-        assert!(v.is_vector());
-        assert!(!v.is_point());
-    }
-
-    #[test]
-    fn new_point() {
-        let p = tuple::point(4.0, -4.0, 3.0);
-        assert!(!p.is_vector());
-        assert!(p.is_point());
-    }
-}
--- src/tuple.rs
+++ src/tuple.rs
@@ -25,3 +25,44 @@ pub fn vector(x: f64, y: f64, z: f64) -> Tuple {
 pub fn point(x: f64, y: f64, z: f64) -> Tuple {
     Tuple { x: x, y: y, z: z, w: 1.0 }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_point() {
+        let a = Tuple { x: 4.3, y: -4.2, z: 3.1, w: 1.00000001 };
+        assert!(a.is_point());
+        assert!(!a.is_vector());
+    }
+
+    #[test]
+    fn is_vector() {
+        let a = Tuple { x: 4.3, y: -4.2, z: 3.1, w: 0.00000001 };
+        assert!(!a.is_point());
+        assert!(a.is_vector());
+    }
+
+    #[test]
+    fn new_vector() {
+        let v = vector(4.0, -4.0, 3.0);
+        assert!(v.is_vector());
+        assert!(!v.is_point());
+    }
+
+    #[test]
+    fn new_point() {
+        let p = point(4.0, -4.0, 3.0);
+        assert!(!p.is_vector());
+        assert!(p.is_point());
+    }
+
+    #[test]
+    fn are_equal() {
+        let t1 = Tuple { x: 1.0, y: 1.0, z: 1.0, w: 1.0 };
+        let t2 = Tuple { x: 2.0, y: 2.0, z: 2.0, w: 2.0 };
+        assert!(!t1.equal(t2));
+    }
+
+}
